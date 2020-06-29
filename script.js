c1cb2665a1db7f73fed62ef95c9b0f59005d1d67
CarmenSR <67562039+CarmenSR@users.noreply.github.com>
2020-06-29 18:41:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+
 function boton(){
   var nombre = document.getElementById("name").value;
   var come = document.getElementById("pp").value;
-
   var p = document.getElementById("post");
   post.innerHTML = post.innerHTML + "<br>";
   post.innerHTML = post.innerHTML + nombre + ": " + come;
 }
+
+
+
+class usuarios{
+  constructor(nombre){
+    this.nombre = nombre;
+  }}
+  var usu = new usuarios ("")
