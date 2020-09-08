6731eaa99a007c252262bd1b0ddb9cb3b2f9cd55
CarmenSR <67562039+CarmenSR@users.noreply.github.com>
2020-09-08 17:11:11 -0500

    Update juego.js

--- periquitos/juego.js
+++ periquitos/juego.js
@@ -1,3 +1,6 @@
+////Este codigo aún tiene errores y está en su etapa de desarrollo
+
+
 document.getElementById("t1").style.display = "none"
 document.getElementById("w").style.display = "none"
 document.getElementById("e").style.display = "none"
@@ -8,17 +11,24 @@ document.getElementById("pc").style.display = "none"
 document.getElementById("yt").style.display = "none"
 document.getElementById("ok").style.display = "none"
 document.getElementById("pro").style.display = "none"
+document.getElementById("pre").style.display = "none"
+document.getElementById("pra").style.display = "none"
+document.getElementById("yasi").style.display = "none"
+
+
 
 function but1() {
 document.getElementById("bnv").innerHTML = "Genial! Ahora elige tu periquito"
 document.getElementById("buta").style.display = "none"
 document.getElementById("butb").style.display = "none"
 document.getElementById("t1").style.display = "block"
+
 };
 function but2 () {
   document.getElementById("bnv").innerHTML = "Bueno, vuelve cuando ya quieras adoptar alguno"
   document.getElementById("buta").style.display = "none"
   document.getElementById("butb").style.display = "none"
+  document.getElementById("yasi").style.display = "block"
 };
 
 function el1 () {
@@ -28,7 +38,7 @@ function el1 () {
   document.getElementById("e").style.display = "block"
   document.getElementById("b").style.display = "block"
   document.getElementById("pb").style.display = "block"
-  document.getElementById("yt").style.display = "block"
+
 
 };
 function el2 () {
@@ -38,7 +48,8 @@ function el2 () {
   document.getElementById("e").style.display = "block"
   document.getElementById("b").style.display = "block"
   document.getElementById("pa").style.display = "block"
-  document.getElementById("yt").style.display = "block"
+
+
 };
 function el3 () {
   document.getElementById("w").style.display = "block"
@@ -47,11 +58,13 @@ function el3 () {
   document.getElementById("e").style.display = "block"
   document.getElementById("b").style.display = "block"
   document.getElementById("pc").style.display = "block"
-  document.getElementById("yt").style.display = "block"
+
+
 };
 function butn() {
   var e = document.getElementById("e").value;
   var qb = document.getElementById("qb");
+  document.getElementById("yt").style.display = "block";
   document.getElementById("qb").innerHTML = "Que bonito nombre es " + e
 };
 function tank() {
@@ -61,11 +74,16 @@ document.getElementById("w").style.display = "none"
 document.getElementById("b").style.display = "none"
 document.getElementById("ok").style.display = "block"
 document.getElementById("pro").style.display = "block"
-document.getElementById("qb").innerHTML = "Aqui tienes las barras de estatus de tu periquito, mantenlas lo mejor que puedas"
-}
+document.getElementById("pre").style.display = "block"
+document.getElementById("pra").style.display = "block"
+document.getElementById("qb").innerHTML = "Arriba tienes las barras de estatus de tu periquito, mantenlas lo mejor que puedas"
+};
+///function anime (){
+  ///document.getElementById("pro").classList.toggle ("final");
+
 function oki() {
   document.getElementById("qb").style.display = "none"
   document.getElementById("ok").style.display = "none"
-
+  ///anime()
 }
 
